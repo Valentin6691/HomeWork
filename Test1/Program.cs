eb23fffb5c66e616489516126f7e0333a777e742
Valentin <nitnelav6691@gmail.cov>
2023-12-16 21:55:55 +0300

    Тест

---
+++ Test1/Program.cs
@@ -0,0 +1,32 @@
+﻿//Бесконечный цикл.
+while(true)
+{
+// Ввод числа
+Console.WriteLine("Введите букву q или число с четной суммоу для прекращения работы программы");
+//Получение данных с консоли.
+string input = Console.ReadLine();
+//Проверка является введенное сообщение "q"
+if (input.ToLower() == "q")
+{
+Console.WriteLine("STOP");
+break;
+}
+//Расчет суммы числа
+int number = Convert.ToInt32(input);
+int sum = 0;
+while(number>0)
+{
+sum = sum + number%10;
+number = number/10;
+}
+//Проверка четности суммы числа
+if(sum%2 == 0)
+{
+Console.WriteLine("STOP")
+break;    
+}
+else
+{
+Console.WriteLine("Сумма цифр числа нечетная");
+}
+}
