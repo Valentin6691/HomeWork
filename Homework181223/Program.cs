6a8c51e22cb5dedbbebf9110cfd17ccd0d4a0e4e
Valentin <nitnelav6691@gmail.cov>
2023-12-16 16:28:08 +0300

    Задание 2

--- Homework181223/Program.cs
+++ Homework181223/Program.cs
@@ -61,3 +61,19 @@
 // трёхзначными числами. Напишите программу, 
 // которая покажет количество чётных чисел в 
 // массив
+
+Console.WriteLine("Введите размер массива");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[num];
+int result = 0;
+for(int i = 0; i<array.Length; i++)
+{
+    array[i] = new Random().Next(100,1000);
+    Console.Write(array[i] + " ");
+    if(array[i]%2 == 0)
+    {
+        result = result + 1;
+    }
+}
+Console.WriteLine("");
+Console.WriteLine(result);
