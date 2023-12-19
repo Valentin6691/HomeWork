b5b2caca2fcdf729ca4b825f351250744798f113
Valentin <nitnelav6691@gmail.cov>
2023-12-19 18:54:09 +0300

    String

--- Homework251223/Program.cs
+++ Homework251223/Program.cs
@@ -17,5 +17,52 @@ string GetStringFromArray()
     }
     return str;
 }
-
 Console.WriteLine(GetStringFromArray());
+
+// Задача 2: Задайте строку, содержащую латинские буквы 
+// в обоих регистрах. Сформируйте строку, в которой все 
+// заглавные буквы заменены на строчные. 
+
+Console.WriteLine("Введите строку");
+string userInput = Console.ReadLine();
+userInput = userInput.ToLower();
+Console.WriteLine(userInput);
+
+// Задача 3: Задайте произвольную строку. Выясните, 
+// является ли она палиндромом.
+
+Console.WriteLine("Введите строку");
+string userInput = Console.ReadLine();
+bool IsPalindrome(string str)
+{
+    for (int i = 0, j = userInput.Length - 1; i < j; i++, j--)
+    {
+        if(userInput[i] != userInput[j])
+        {
+            return false;
+        }
+         
+    }
+return true;
+}
+if (IsPalindrome(userInput))
+        {
+            Console.WriteLine("Да");
+        }
+        else
+        {
+            Console.WriteLine("Нет");
+        }
+
+// Задача 4*(не обязательная): Задайте строку, состоящую 
+// из слов, разделенных пробелами. Сформировать строку, 
+// в которой слова расположены в обратном порядке. В 
+// полученной строке слова должны быть также разделены 
+// пробелами.
+
+Console.Write("Введите строку: ");
+string input = Console.ReadLine();
+string[] words = input.Split(' ');
+Array.Reverse(words);
+string reversedString = string.Join(" ", words);
+Console.WriteLine("Результат: " + reversedString);
